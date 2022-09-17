90329ee501420596bb3d2b41a4df80d698703b8d
carolyn.sher <carolyn.sher@wgu.edu>
2022-09-17 05:30:37 -0400

    Fixed D387 not add more rooms every time runs

--- src/main/java/edu/wgu/d387_sample_code/H2Bootstrap.java
+++ src/main/java/edu/wgu/d387_sample_code/H2Bootstrap.java
@@ -19,10 +19,21 @@ public class H2Bootstrap implements CommandLineRunner {
 		// TODO Auto-generated method stub
 		
 		System.out.println("Bootstrapping data: ");
+		RoomEntity room1= new RoomEntity(405, "200");
+		room1.setId(1L);
 		
-		roomRepository.save(new RoomEntity(405, "200"));
-		roomRepository.save(new RoomEntity(406, "220"));
-		roomRepository.save(new RoomEntity(407, "250"));
+		roomRepository.save(room1);
+
+		RoomEntity room2= new RoomEntity(406, "220");
+		room2.setId(2L);
+
+		roomRepository.save(room2);
+
+		RoomEntity room3= new RoomEntity(407, "260");
+		room3.setId(3L);
+
+		roomRepository.save(room3);
+
 		
 		Iterable<RoomEntity> itr = roomRepository.findAll();
 		
